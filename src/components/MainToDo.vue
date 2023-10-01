dc16bddd27ed6a57fd87f891d77187024c159a14
taroyaka <skai1997718@gmail.com>
2023-10-01 10:21:36 +0900

    TODOのリスト化

--- src/components/MainToDo.vue
+++ src/components/MainToDo.vue
@@ -1,59 +1,106 @@
 <script setup>
-    import { ref } from 'vue';
-    const todoRef = ref('');
-    const todoListRef = ref([]);
+import { ref } from 'vue';
+const todoRef = ref('');
+const todoListRef = ref([]);
 
-    const addTodo = () => {
+const addTodo = () => {
+  // IDをミリ秒単位で登録
+  const id = new Date().getTime();
 
-        // IDをミリ秒単位で登録
-        const id = new Date().getTime();
+  // 配列に入力TODOを格納
+  todoListRef.value.push({ id: id, task: todoRef.value });
 
-        // 配列に入力TODOを格納
-        todoListRef.value.push({ id: id, task: todoRef.value });
+  // ローカルストレージに登録
+  // localStorage.todoList.push(JSON.stringify(todoRef.value));
+  localStorage.todoList = JSON.stringify(todoListRef.value);
 
-        // ローカルストレージに登録
-        // localStorage.todoList.push(JSON.stringify(todoRef.value));
-        localStorage.todoList = (JSON.stringify(todoListRef.value));
-
-
-        // 登録後は入力欄を空にする
-        todoRef.value = '';
-    };
+  // 登録後は入力欄を空にする
+  todoRef.value = '';
+};
 </script>
 
 <template>
-    <div class="box_input">
-        <input 
-            type="text" 
-            class="todo_input" 
-            v-model="todoRef"
-            placeholder="＋ TODOを入力" 
-        />
-        <button class="btn" @click="addTodo">追加</button>
+  <div class="box_input">
+    <input
+      type="text"
+      class="todo_input"
+      v-model="todoRef"
+      placeholder="＋ TODOを入力"
+    />
+    <button class="btn" @click="addTodo">追加</button>
+  </div>
+  <div class="box_list">
+    <div class="todo_list">
+      <div class="todo">
+        <input type="checkbox" class="check" /><label>TODO01</label>
+      </div>
+      <div class="btns">
+        <button class="btn green">編</button>
+        <button class="btn pink">削</button>
+      </div>
     </div>
+    <div class="todo_list">
+      <div class="todo">
+        <input type="checkbox" class="check" /><label>TODO02</label>
+      </div>
+      <div class="btns">
+        <button class="btn green">編</button>
+        <button class="btn pink">削</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-    .box_input{
-        margin-top: 20px;
-    }
-
-    .todo_input{
-        width: 300px;
-        margin-right: 8px;
-        padding: 8px;
-        font-size: 18px;
-        border: lpx solid #aaa;
-        border-radius: 6px;
-        
-    }
-
-    .btn{
-        padding: 8px;
-        background-color: #03a9f4;
-        border-radius: 6px;
-        color: #fff;
-        text-align: center;
-        font-size: 14px;
-    }
+.box_input {
+  margin-top: 20px;
+}
+.todo_input {
+  width: 300px;
+  margin-right: 8px;
+  padding: 8px;
+  font-size: 18px;
+  border: lpx solid #aaa;
+  border-radius: 6px;
+}
+.btn {
+  padding: 8px;
+  background-color: #03a9f4;
+  border-radius: 6px;
+  color: #fff;
+  text-align: center;
+  font-size: 14px;
+}
+.box_list {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+}
+.todo_list {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+}
+.todo {
+  border: 1px solid #ccc;
+  border-radius: 6px;
+  padding: 12px;
+  width: 300px;
+}
+.check {
+  border: 1px solid red;
+  transform: scale(1.6);
+  margin: 0 16px 2px 6px;
+}
+.btns {
+  display: flex;
+  gap: 4px;
+}
+.green {
+  background-color: #00c853;
+}
+.pink {
+  background-color: #ff4081;
+}
 </style>
